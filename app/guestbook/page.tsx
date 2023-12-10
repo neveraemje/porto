d8fdb323e436bb752285dfbefda4d4572b1f15d6
neveraemje <neveraemje@gmail.com>
2023-12-10 23:03:56 +0700

    fix guest 3

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
 const guestData = async() => {
     try {
 
-        const res = await fetch('http://localhost:3000/api/user', {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user`, {
             cache: "no-cache"
         })
 
